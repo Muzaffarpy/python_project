68a0fe70e3cc6109b36ae0461e832a4f319c5859
Muzaffarpy <157621275+Muzaffarpy@users.noreply.github.com>
2024-02-26 08:15:23 -0800

    Update mainu.py

--- mainu.py
+++ mainu.py
@@ -2,15 +2,7 @@
 import streamlit as st
 import pandas as pd
 
-def main():
-   menu()
-
-
-def menu():
-    print("************Welcome to FakeFlix Demo**************")
-    print()
-
-    choice = input("""
-                      A: Please Register
-                      B: Login
-                      Q: Logout
+menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+if choice == "Home":
+        st.subheader("Home")
