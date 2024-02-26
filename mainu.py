0a39235aee7b7501fe412a54e5ab6f01244cd157
Muzaffarpy <157621275+Muzaffarpy@users.noreply.github.com>
2024-02-26 08:38:57 -0800

    Update mainu.py

--- mainu.py
+++ mainu.py
@@ -3,12 +3,9 @@ import pandas as pd
 
 # Sidebar menu
 st.sidebar.title("Menu")
-selected_page = st.sidebar.radio("", ["Address Book", "About Us", "Contact"])
+selected_page = st.sidebar.radio("", [ "About Us", "Contact"])
 
-if selected_page == "Address Book":
-    st.title("Address Book")
-    # Add address book functionality here
-elif selected_page == "About Us":
+if selected_page == "About Us":
     st.title("About Us")
     st.write(f"This address book application was created by {name}.")
 elif selected_page == "Contact":
